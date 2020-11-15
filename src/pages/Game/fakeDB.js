5b5fa141fe3a8719de731914c463ea648823f655
Si Choi <0334pc@gmail.com>
2020-11-15 10:26:40 +0900

    [ADDED] | added fakeDB to Game pages for testing

---
+++ src/pages/Game/fakeDB.js
@@ -0,0 +1,30 @@
+export default {
+  DRAWING: {
+    curRound: 3,
+    totalRound: 6,
+    limitTime: 20,
+    handleTimeOut: () => "timeout",
+    currentPlayer: {
+      player_id: "siDaBest",
+      username: "Si",
+      avatar: "AVATAR_KIWI",
+    },
+    playersList: [
+      { player_id: "kjeDaBest", username: "kje", avatar: "AVATAR_KIWI" },
+      { player_id: "dongocDaBest", username: "dongoc", avatar: "AVATAR_KIWI" },
+      { player_id: "paulDaBest", username: "paul", avatar: "AVATAR_KIWI" },
+      {
+        player_id: "masterSongDaBest",
+        username: "masterSong",
+        avatar: "AVATAR_KIWI",
+      },
+      {
+        player_id: "developerKjeDaBest",
+        username: "developerKje",
+        avatar: "AVATAR_KIWI",
+      },
+      { player_id: "siDaBest", username: "Si", avatar: "AVATAR_KIWI" },
+    ],
+    preGuessWord: "망고 프라푸치노",
+  },
+};
