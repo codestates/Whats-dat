f4e565d7ff5a9a54ca6b5d86069869b9e411782d
dkje <developer.kje@gmail.com>
2020-11-16 16:12:47 +0900

    [Added] | render components by round

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Drawing from "./Drawing";
-import fakeDB from "./fakeDB";
+import GuessWord from "./GuessWord";
+import SelectWord from "./SelectWord";
+import GameResults from "./GameResults";
+import { useGame } from "../../contexts/GameContext";
+
+// TODO: 라운드 진행(GameContext)에 따라 다른 컴포넌트 표시
 
 const index = () => {
-  return (
-    <>
-      <Drawing {...fakeDB.DRAWING} />
-    </>
-  );
+  const { gameLog } = useGame();
+  const [currentRound, setCurrentRound] = useState();
+
+  const wordList = ["원할머니 보쌈", "아련한 강아지", "포카칩"];
+  const onSubmit = (values) => {
+    console.log("submit!", values.word);
+    // firebase
+  };
+
+  const handleTimeOut = (values) => {
+    onSubmit(values);
+  };
+
+  useEffect(() => {
+    // TODO
+    if (!gameLog) return;
+    if (gameLog.rounds.length) {
+      setCurrentRound(gameLog.rounds.length - 1);
+    }
+  }, [gameLog]);
+
+  const renderCurrentRound = () => {
+    if (currentRound === undefined) {
+      return null;
+    }
+
+    if (currentRound === 0) {
+      console.log("round 0 start");
+      return <SelectWord wordList={wordList} handleTimeOut={handleTimeOut} />;
+    }
+
+    if (currentRound % 2 === 1) {
+      console.log("odd round start");
+      return <Drawing />;
+      //   curRound,
+      // totalRound,
+      // limitTime,
+      // handleTimeOut,
+      // currentPlayer,
+      // playersList,
+      // preGuessWord,
+    }
+
+    if (currentRound % 2 === 0) {
+      console.log("even round start");
+      return <GuessWord />;
+      //   curRound,
+      // totalRound,
+      // limitTime,
+      // handleTimeOut,
+      // currentPlayer,
+      // playersList,
+      // imageUrl,
+    }
+
+    return new Error("invalid round count");
+  };
+
+  return <>{renderCurrentRound()}</>;
 };
 
 export default index;
